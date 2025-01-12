75b896e49066320df437cff47228702dc3008ba9
Samanehbeik <samaneh.alibeik@gmail.com>
2025-01-12 14:26:43 +0330

    v3

--- scripts.js
+++ scripts.js
@@ -24,43 +24,44 @@ function addValueByKeys() {
   if (emptyCells.length > 0) {
     var randomEmptyCell = Math.floor(Math.random() * emptyCells.length);
     emptyCells.eq(randomEmptyCell).text(2).css("background-color", "#FFEFD5");
-  } else {
-    console.log("No empty cells available.");
+  }
+}
+
+function addDown(index, k, j) {
+  let $row1 = $rows.eq(index).find("td").eq(j);
+  let $row2 = $rows
+    .eq(index + k)
+    .find("td")
+    .eq(j);
+  let t = 0;
+  if ($row2.text().trim() == $row1.text().trim()) {
+    t = $row2.text() + $row1.text();
+    return [$row1.text(""), $row2.text(t)];
   }
 }
 
 function keydown() {
-  for (var i = 0; i < $rows.length - 1; i += 2) {
-    var $row1 = $rows.eq(i).find("td");
-    var $row2 = $rows.eq(i + 1).find("td");
-    $row1.each(function (index) {
-      var cellValue1 = $(this).text().trim();
-      var cellValue2 = $row2.eq(index).text().trim();
-      if (cellValue1 === cellValue2) {
-        console.log(
-          "Row " +
-            i +
-            " and Row " +
-            (i + 1) +
-            " - Cell " +
-            index +
-            " is equal: " +
-            cellValue1
-        );
-      } else {
-        console.log(
-          "Row " +
-            i +
-            " and Row " +
-            (i + 1) +
-            " - Cell " +
-            index +
-            " is not equal: " +
-            cellValue1 +
-            " vs " +
-            cellValue2
-        );
+  for (var j = 0; j < 4; j++) {
+    let i = 0;
+    while (i < 3) {
+      let $row1 = $rows.eq(i).find("td").eq(j);
+      let $row2 = $rows.eq(i + 1).find("td").eq(j);
+      let $row3 = $rows.eq(i + 2).find("td").eq(j);
+        if ($row1.text().trim() !== " ") {
+        if ($row2.text().trim() !== " ") {
+          addDown(i, 1, j);
+          i++;
+        } else {
+          if ($row3.text().trim() !== "") {
+            addDown(i, 2, j);
+            i++;
+          } else {
+            let $row4 = $rows.eq(3).find("td").eq(j);
+            $row4.text($row1.text());
+          }
+        }
       }
-    });
+      i++;
+    }
   }
 }

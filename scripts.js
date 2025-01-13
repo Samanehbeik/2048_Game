f35b8d0324d49c4e04c67da46393b4e9fa8571ed
Samanehbeik <samaneh.alibeik@gmail.com>
2025-01-13 18:45:27 +0330

    v6

--- scripts.js
+++ scripts.js
@@ -24,50 +24,59 @@ function addValueByKeys() {
   if (emptyCells.length > 0) {
     var randomEmptyCell = Math.floor(Math.random() * emptyCells.length);
     emptyCells.eq(randomEmptyCell).text(2).css("background-color", "#FFEFD5");
+  } else {
+    alert("You Lose the Game!");
   }
 }
 
-function addDown(index, k, j) {
-  let row1 = $("#myTable tr:nth-child("+index+") td:nth-child("+j+")");
-  let row2 = $("#myTable tr:nth-child("+index+k+") td:nth-child("+j+")");
-  let t = row2.text() + row1.text();
-    row1.text("");
-    row2.text(t);
-   }
- 
+// function addDown(index, k, j) {
+//   let row1 = $("#myTable tr:nth-child(" + index + ") td:nth-child(" + j + ")");
+//   let row2 = $(
+//     "#myTable tr:nth-child(" + index + k + ") td:nth-child(" + j + ")"
+//   );
+//   let t = row2.text() + row1.text();
+//   row1.empty();
+//   row2.text("newValue");
+// }
+
 function keydown() {
-  for (var j = 0; j < 4; j++) {
-    let i = 0;
-    while (i < 3) {
-      // let $row1 = $rows.eq(i).find("td").eq(j);
-      // let row1 = $rows.eq(i).children('td').eq(j);
-      let row1 = $("#myTable tr:nth-child("+i+") td:nth-child("+j+")");
-      let row2 = $("#myTable tr:nth-child("+i+1+") td:nth-child("+j+")");     
-      let row3 = $("#myTable tr:nth-child("+i+2+") td:nth-child("+j+")"); 
-      let row4 = $("#myTable tr:nth-child("+i+3+") td:nth-child("+j+")");
-     
-      console.log(row1.text()+row2.text());
-       if (row1.text()!="") {
-          if (row2.text() == row1.text()) {
-           addDown(i, 1, j);
-          i++;
-        } else {
-          if (row3.text().trim() != "" && i<2) {
-            if (row2.text()== row1.text()){addDown(i, 2, j);
-            i++;}
-          } else {
-            if (row4.text().trim() == row1.text().trim()) {
-              t = row4.text() + row1.text();
-              row1.text("");
-              row4.text(t);
+  for (var j = 0; j < 1; j++) {
+    for (var i = 3; i >= 0; i--) {
+      let row0 = $("#cell" + 3 + "-" + j + "").text();
+      let row1 = $("#cell" + 2 + "-" + j + "").text();
+      let row2 = $("#cell" + 1 + "-" + j + "").text();
+      let row3 = $("#cell" + 0 + "-" + j + "").text();
+      console.log("0 is " + row0);
+      console.log("1 is " + row1);
+      console.log("2 is " + row2);
+      console.log("3 is " + row3);
+
+      if (!$.trim(row0)) {
+        if (!$.trim(row1)) {
+          if (!$.trim(row2)) {
+            if ($.trim(row3)) {
+              $("#cell" + i + "-" + j + "").text(row3);
+              $("#cell" + i - 3 + "-" + j + "").text("");
             }
-            else {
-              row4.text(row1.text());
-            }//
+          } else {
+            $("#cell" + i + "-" + j + "").text(row2);
+            $("#cell" + i - 2 + "-" + j + "").text("");
           }
+        } else {
+          $("#cell" + i + "-" + j + "").text(row1);
+          $("#cell" + i - 1 + "-" + j + "").text("");
         }
-              }
-       i++;
+      }
     }
+  }
+}
+
+function keydown2() {
+  for (var j = 0; j < 1; j++) {
+    let row0 = $("#cell" + 3 + "-" + j + "").text();
+    let row1 = $("#cell" + 2 + "-" + j + "").text();
+    let row2 = $("#cell" + 1 + "-" + j + "").text();
+    let row3 = $("#cell" + 0 + "-" + j + "").text();
+
   }
 }
---
+++ text.js
@@ -0,0 +1,31 @@
+ // row2 =$("#"+i+1+"-"+j+"").text();
+          // row3 =$("#"+i+2+"-"+j+"").text();
+          // row4 =$("#"+i+3+"-"+j+"").text();
+        
+      // let row1 = $rows.eq(i).children('td').eq(j);
+      // let row1 = $("#myTable tr:nth-child("+i+") td:nth-child("+j+")");
+      // let row2 = $("#myTable tr:nth-child("+i+1+") td:nth-child("+j+")");     
+      // let row3 = $("#myTable tr:nth-child("+i+2+") td:nth-child("+j+")"); 
+      // let row4 = $("#myTable tr:nth-child("+i+3+") td:nth-child("+j+")");
+          //  if (row1.text()!=" ") {
+           
+      //     if (row2.text() == row1.text()) {
+      //     // 
+          
+      //     i++;
+      //   } else {
+      //     if (row3.text().trim() != "" && i<2) {
+      //       if (row2.text()== row1.text()){addDown(i, 2, j);
+      //       i++;}
+      //     } else {
+      //       if (row4.text().trim() == row1.text().trim()) {
+      //         t = row4.text() + row1.text();
+      //         row1.text("");
+      //         row4.text(t);
+      //       }
+      //       else {
+      //         row4.text(row1.text());
+      //       }//
+      //     }
+      //   }
+      //         }

e02c81f615e35b25500002ee9f5076db8f919410
Samanehbeik <samaneh.alibeik@gmail.com>
2025-01-21 12:57:39 +0330

    v15

--- test.js
+++ test.js
@@ -1,12 +1,30 @@
 var $rows = $("#myTable tr");
+function ColorId(value) {
+  var colorMap = {
+    2: "#FFEFD5",
+    4: "#FFDAB9",
+    8: "#FF7F50",
+    16: "#FF5722",
+    32: "#F4511E",
+    64: "#DD2C00",
+    128: "#FF3D00",
+    256: "#FFCA28",
+    512: "#FFB300",
+    1024: "#FFA000",
+    2043: "#FF9100",
+  };
+  return colorMap[value] || "Invalid input";
+}
 function StartNewGame() {
   var $cells = $("#myTable td");
   $cells.text(" ");
+  $cells.attr("LockValue", "false");
   $cells.css("background-color", "rgb(205 193 180");
   var shuffledCells = $cells
     .sort(function () {
       return 0.5 - Math.random();
     })
+
     .slice(0, 2);
   shuffledCells.each(function () {
     var randomValue = Math.random() < 0.5 ? 2 : 4;
@@ -14,6 +32,21 @@ function StartNewGame() {
     var color = randomValue == 2 ? "#FFEFD5" : "#FFDAB9";
     $(this).css("background-color", color);
   });
+}
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}m:${remainingSeconds}s`;
+}
+function timer() {
+  var sec = 0;
+  var timer = setInterval(function () {
+    document.getElementById("safeTimerDisplay").innerHTML = formatTime(sec);
+    sec++;
+    if (sec < 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
 
 function addValueByKeys() {
@@ -28,69 +61,18 @@ function addValueByKeys() {
     alert("You Lose the Game!");
   }
 }
-function AddReplace(i, j) {
-  let Color = ColorId(2 * i.text());
-  let ColorR = ColorId(j.text());
-  if (i.text() == j.text()) {
-    i.text(2 * i.text()).css("background-color", Color);
-    j.text("").css("background-color", "#CDC1B4");
-    i.attr("LockValue", "true");
-  }
-  if (!$.trim(i.text())) {
-    i.text(j.text()).css("background-color", ColorR);
-    j.text("").css("background-color", "#CDC1B4");
-  }
-}
-// function move(Arr) {
-//   if ($.trim(Arr[1].text())) {
-//     AddReplace(Arr[0], Arr[1]);
-//   }
-//   if ($.trim(Arr[2].text())) {
-//     if (Arr[0].attr("LockValue") == false && !$.trim(Arr[1].text())) {
-//       Arr[0] == Arr[2]
-//         ? AddReplace(Arr[0], Arr[2])
-//         : AddReplace(Arr[1], Arr[2]);
-//     } else {
-//       AddReplace(Arr[1], Arr[2]);
-//     }
-//   }
-//   if ($.trim(Arr[3].text())) {
-//     if (!$.trim(Arr[2].text())) {
-//       if (!$.trim(Arr[1].text())) {
-//         if (Arr[0].attr("LockValue") == false) {
-//           if (Arr[0] == Arr[3] || !$.trim(Arr[0].text())) {
-//             AddReplace(Arr[0], Arr[3]);
-//           } else {
-//             AddReplace(Arr[1], Arr[3]);
-//           }
-//         }
-//       } else {
-//         if (Arr[1].attr("LockValue") == false) {
-//           Arr[1] == Arr[3]
-//             ? AddReplace(Arr[1], Arr[3])
-//             : AddReplace(Arr[2], Arr[3]);
-//         }
-//       }
-//     } else {
-//       AddReplace(Arr[2], Arr[3]);
-//       if (!$.trim(Arr[0].text()) && !$.trim(Arr[1].text())) {
-//         AddReplace(Arr[0], Arr[2]);
-//       }
-//       if ($.trim(Arr[0].text()) && !$.trim(Arr[1].text())) {
-//         AddReplace(Arr[1], Arr[2]);
-//       }
-//     }
-//   }
-// }
+
 function move(row1, row2) {
+  // console.log(row1.attr("LockValue"));
   let Color = ColorId(2 * row1.text());
   let ColorR = ColorId(row2.text());
+  if (row1.attr("LockValue") == "true" || row2.attr("LockValue") == "true") {
+    return;
+  }
   if (!$.trim(row2.text())) {
-    return
+    return;
   }
-  if (row1.attr("LockValue") == true || row2.attr("LockValue") == true){
-    return
-  }
+
   if (row1.text() == row2.text()) {
     row1.text(2 * row1.text()).css("background-color", Color);
     row2.text("").css("background-color", "#CDC1B4");
@@ -119,8 +101,9 @@ function keydown2() {
     Play(arr);
     arr[0].attr("LockValue", "false");
     arr[1].attr("LockValue", "false");
+    arr[2].attr("LockValue", "false");
+    arr[3].attr("LockValue", "false");
   }
-
 }
 
 function keyUp2() {
@@ -131,6 +114,10 @@ function keyUp2() {
     arr[2] = $("#cell" + 2 + "-" + j);
     arr[3] = $("#cell" + 3 + "-" + j);
     Play(arr);
+    arr[0].attr("LockValue", "false");
+    arr[1].attr("LockValue", "false");
+    arr[2].attr("LockValue", "false");
+    arr[3].attr("LockValue", "false");
   }
 }
 
@@ -142,6 +129,10 @@ function keyRight2() {
     arr[2] = $("#cell" + j + "-" + 1);
     arr[3] = $("#cell" + j + "-" + 0);
     Play(arr);
+    arr[0].attr("LockValue", "false");
+    arr[1].attr("LockValue", "false");
+    arr[2].attr("LockValue", "false");
+    arr[3].attr("LockValue", "false");
   }
 }
 function keyLeft2() {
@@ -152,5 +143,9 @@ function keyLeft2() {
     arr[2] = $("#cell" + j + "-" + 2);
     arr[3] = $("#cell" + j + "-" + 3);
     Play(arr);
+    arr[0].attr("LockValue", "false");
+    arr[1].attr("LockValue", "false");
+    arr[2].attr("LockValue", "false");
+    arr[3].attr("LockValue", "false");
   }
 }
--- text.js
+++ text.js
@@ -29,3 +29,111 @@
       //     }
       //   }
       //         }
+
+      // function move(Arr) {
+//   if ($.trim(Arr[1].text())) {
+//     AddReplace(Arr[0], Arr[1]);
+//   }
+//   if ($.trim(Arr[2].text())) {
+//     if (Arr[0].attr("LockValue") == false && !$.trim(Arr[1].text())) {
+//       Arr[0] == Arr[2]
+//         ? AddReplace(Arr[0], Arr[2])
+//         : AddReplace(Arr[1], Arr[2]);
+//     } else {
+//       AddReplace(Arr[1], Arr[2]);
+//     }
+//   }
+//   if ($.trim(Arr[3].text())) {
+//     if (!$.trim(Arr[2].text())) {
+//       if (!$.trim(Arr[1].text())) {
+//         if (Arr[0].attr("LockValue") == false) {
+//           if (Arr[0] == Arr[3] || !$.trim(Arr[0].text())) {
+//             AddReplace(Arr[0], Arr[3]);
+//           } else {
+//             AddReplace(Arr[1], Arr[3]);
+//           }
+//         }
+//       } else {
+//         if (Arr[1].attr("LockValue") == false) {
+//           Arr[1] == Arr[3]
+//             ? AddReplace(Arr[1], Arr[3])
+//             : AddReplace(Arr[2], Arr[3]);
+//         }
+//       }
+//     } else {
+//       AddReplace(Arr[2], Arr[3]);
+//       if (!$.trim(Arr[0].text()) && !$.trim(Arr[1].text())) {
+//         AddReplace(Arr[0], Arr[2]);
+//       }
+//       if ($.trim(Arr[0].text()) && !$.trim(Arr[1].text())) {
+//         AddReplace(Arr[1], Arr[2]);
+//       }
+//     }
+//   }
+// }
+// function AddReplace(i, j) {
+//   let Color = ColorId(2 * i.text());
+//   let ColorR = ColorId(j.text());
+//   if (i.text() == j.text()) {
+//     i.text(2 * i.text()).css("background-color", Color);
+//     j.text("").css("background-color", "#CDC1B4");
+//     i.attr("LockValue", "true");
+//   }
+//   if (!$.trim(i.text())) {
+//     i.text(j.text()).css("background-color", ColorR);
+//     j.text("").css("background-color", "#CDC1B4");
+//   }
+// }
+// function cellValue(index) {
+    //   const arr = [];
+    //   for (j = 0; j < 4; j++) {
+    //         arr[j] = $("#cell" + j + "-" + index).text();
+          
+    //   }
+    //   return arr;
+    // }
+    
+    // function addDown(i, k, j) {
+    //   let row = $("#cell" + i + "-" + j).text();
+    //   $("#cell" + k + "-" + j)
+    //     .text("")
+    //     .css("background-color", "#CDC1B4");
+    //   let Color = ColorId(2 * row);
+    //   return $("#cell" + i + "-" + j)
+    //     .text(2 * row)
+    //     .css("background-color", Color);
+    
+    // }
+    
+    // function replace(i, k, j) {
+    //   let row = $("#cell" + k + "-" + j).text();
+    //   let rowColor = $("#cell" + k + "-" + j).css("background-color");
+    //   $("#cell" + i + "-" + j)
+    //     .text(row)
+    //     .css("background-color", rowColor);
+    //   $("#cell" + k + "-" + j)
+    //     .text(" ")
+    //     .css("background-color", "#CDC1B4");
+    // }
+    // function addL(i, k, j) {
+    //   let row = $("#cell" + j + "-" + i).text();
+    //   $("#cell" + j + "-" + k)
+    //     .text("")
+    //     .css("background-color", "#CDC1B4");
+    //   let Color = ColorId(2 * row);
+    //   return $("#cell" + j + "-" + i)
+    //     .text(2 * row)
+    //     .css("background-color", Color);
+    // }
+    
+    // function replaceL(i, k, j) {
+    //   let row = $("#cell" + j + "-" + k).text();
+    //   let rowColor = $("#cell" + j + "-" + k).css("background-color");
+    //   $("#cell" + j + "-" + i)
+    //     .text(row)
+    //     .css("background-color", rowColor);
+    //   $("#cell" + j + "-" + k)
+    //     .text("")
+    //     .css("background-color", "#CDC1B4");
+    // }
+    
